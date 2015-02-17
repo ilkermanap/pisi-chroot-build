791a26b6fdc1cff98a6a14c9f24d48c857816ea7
ilkermanap <ilkermanap@gmail.com>
2015-02-17 16:17:19 +0100

    hala hatalar var.. paket adi  yerine dosya isimlerini kullanmak daha kolay olabilir..

--- pisiroot.py
+++ pisiroot.py
@@ -9,7 +9,7 @@ Used the script above.
 global CACHEDIR
 CACHEDIR= "/var/cache/pisi/packages"
 
-BASE = "acl attr baselayout bash binutils catbox coreutils file gawk gcc glibc glibc-devel gmp grep kernel-headers libcap  libffi libgcc libmpc libpcre make mpfr ncurses openssl pisi python readline sed zlib autoconf automake diffutils   gnuconfig libtool piksemel ca-certificates  comar-api curl gperftools  leveldb  libgcrypt libgpg-error libidn      libssh2 libunwind pisilinux-dev-tools pisilinux-python  plyvel pycurl python python3 python-pyliblzma run-parts    snappy urlgrabber xz"
+BASE = "acl attr baselayout bash binutils bzip2 expat catbox coreutils file gawk gcc glibc glibc-devel gmp grep kernel-headers libcap  libffi libgcc libmpc libpcre make mpfr ncurses openssl pisi python readline sed zlib autoconf automake diffutils   gnuconfig libtool piksemel ca-certificates  comar-api curl gperftools  leveldb  libgcrypt libgpg-error libidn dbus     libssh2 libunwind pisilinux-dev-tools pisilinux-python  plyvel pycurl python python3 python-pyliblzma run-parts    snappy urlgrabber xz"
 
 
 class PisiPackage:
@@ -21,22 +21,42 @@ class PisiPackage:
     """
     def __init__(self, pname):
         self.filename = self.findInCache(pname)
+        self.name = pname
         if self.filename == False:
             self.retrieve(pname)
             self.filename = self.findInCache(pname)
+
+
+    def checkFile(self, fname, pname):
+        print "packages, ",fname, pname
+        f = os.popen("pisi info  %s" % fname).readlines()
+        print "2"
+        for line in f:
+            if line.find("Name") > -1:
+                fpkgname = line.split(":")[1].split(",")[0].strip()
+                if pname == fpkgname:
+                    return True
+        return False
 
     def findInCache(self, name):
         """
         Check if the package file is in cache
         return filename if found, 
         else return False
+
+
+        Package file        : ncurses-devel-5.9-5-p01-x86_64.pisi
+        Name                : ncurses-devel, version: 5.9, release: 5
+
         """
-        files = glob.glob("%s/%s*" % (CACHEDIR, name))
+        files = glob.glob("%s/%s-[0-9]*" % (CACHEDIR, name))
         if len(files) > 0:
+            print files[-1], name
+            #inf = os.popen("pisi info %s" % files[-1]).readlines()
+            #print inf[1]
             latest = files[-1]
             return latest
-        else:
-            return False
+        return False
 
     def retrieve(self, name):
         """
@@ -54,7 +74,7 @@ class PisiPackage:
         In default it will ignore dependencies.
         if you don't want to ignore dependencies,
         use it as below:
-        
+
             pkg.install("/tmp/rootfs", ignoredep = False)
 
         With ignoredep = True:
@@ -63,14 +83,14 @@ class PisiPackage:
         """
         cmd = "pisi it --ignore-comar"
         if ignoredep == True:
-            cmd += " --ignore-dep" 
+            cmd += " --ignore-dep"
         cmd += " -D %s -y %s  " % (basedir, self.filename)
         os.system(cmd)
 
 class Packages:
     """
     This class allows us to define package groups.
-    Just pass the name of packages in a string separated 
+    Just pass the name of packages in a string separated
     with whitespace.
 
     games = Packages("boswars tetris")
@@ -88,10 +108,13 @@ class Packages:
         for pkg in pkgs:
             self.packages[pkg] = PisiPackage(pkg)
 
+    def addPackage(self, name):
+        self.packages[name] = PisiPackage(name)
 
 class RootFS:
     def __init__(self):
-        self.base = Packages(BASE)
+        self.base = Packages(liststr = BASE)
+        self.buildDeps = Packages()
         self.repo = "http://farm.pisilinux.org/.nofarm-repo/x86_64/pisi-index.xml.xz"
         self.cache = "/var/cache/pisi/packages"
         os.system("mkdir -p %s" % self.cache)
@@ -110,6 +133,12 @@ class RootFS:
         self.copyFiles()
         self.mknods()
         self.runCommand("pisi ar pisirepo %s" % self.repo)
+        self.runCommand("dbus-uuidgen --ensure")
+        self.runCommand("dbus-daemon --system")
+        self.runCommand("chmod o+x /usr/lib/dbus-1.0/dbus-daemon-launch-helper")
+        self.runCommand("pisi configure-pending")
+
+
 
     def mknods(self):
         self.runCommand("mknod /dev/console c 5 1")
@@ -124,6 +153,7 @@ class RootFS:
     def installBase(self):
         for pkgname, pkg in self.base.packages.items():
             pkg.install(self.rootdir)
+            print pkg.filename
 
     def runCommand(self, cmd):
         os.system("chroot %s  %s" % (self.rootdir, cmd) )
@@ -147,6 +177,21 @@ class RootFS:
     def clean(self):
         self.mountDirs(umount = True)
 
+    def findBuildDeps(self):
+
+        from xml.dom import minidom
+
+        pspec = minidom.parse("%s/root/pkg/pspec.xml" % self.rootdir)
+        alldeps = pspec.getElementsByTagName("BuildDependencies")
+        deps = alldeps[0].getElementsByTagName("Dependency")
+        for d in deps:
+            deppkg = d.toxml().split(">")[1].split("<")[0]
+            self.buildDeps.addPackage(deppkg)
+
+        for pkgname, pkg in self.buildDeps.packages.items():
+            pkg.install(self.rootdir)
+
+
     def addpkg(self, pkgdir):
         cmd = "cp -r %s/* %s/root/pkg/." % (pkgdir, self.rootdir)
         os.system("mkdir -p %s/root/pkg" % self.rootdir)
@@ -155,5 +200,6 @@ class RootFS:
 if __name__ == "__main__":
     x = RootFS()
     x.addpkg(sys.argv[1])
-    x.runCommand("pisi bi  /root/pkg/pspec.xml")
+    x.findBuildDeps()
+    x.runCommand("pisi  --ignore-action-errors --ignore-safety bi   /root/pkg/pspec.xml")
     x.clean()
