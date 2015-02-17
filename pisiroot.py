afe5ddd3b4d1e5d1beff08b133848d7e6057c6bd
ilkermanap <ilkermanap@gmail.com>
2015-02-17 10:53:49 +0100

    paket ve paketler icin sinif yazildi, test edilmedi

--- pisiroot.py
+++ pisiroot.py
@@ -1,4 +1,3 @@
-
 import os, sys, uuid, syslog, subprocess, glob
 
 """
@@ -7,8 +6,90 @@ https://github.com/evolve-os/repository/blob/master/system/base/pisi/files/evobu
 Used the script above.
 """
 
+global CACHEDIR 
+CACHEDIR= "/var/cache/pisi/packages"
+
+BASE = "acl attr baselayout bash binutils catbox coreutils file gawk gcc glibc glibc-devel gmp grep kernel-headers libcap libffi libgcc libmpc libpcre make mpfr ncurses openssl pisi python readline sed zlib autoconf automake diffutils gnuconfig libtool"
+
+class PisiPackage:
+    """
+    Basic pisi package operations class
+    During init, it will search the local cache,
+    if name isn't found, then it will fetch the 
+    package from remote repository.
+    """
+    def __init__(self, pname):
+        self.filename = self.findInCache(pname)
+        if self.filename == False:
+            self.retrieve(pname)
+        
+    def findInCache(self, name):
+        """
+        Check if the package file is in cache
+        return filename if found, 
+        else return False
+        """
+        files = glob.glob("%s/%s*" % (CACHEDIR, name))
+        if len(files) > 0:
+            latest = files[-1]
+            return latest
+        else:
+            return False
+
+    def retrieve(self, name):
+        """
+        Retrieve the package from repository
+        """
+        currpath = os.getcwd()
+        os.chdir(CACHEDIR)
+        os.system("pisi fc %s" % name)
+        os.chdir(currpath)
+
+    def install(self, basedir, ignoredep = True):
+        """
+        Install the package
+
+        In default it will ignore dependencies.
+        if you don't want to ignore dependencies,
+        use it as below:
+        
+            pkg.install("/tmp/rootfs", ignoredep = False)
+
+        With ignoredep = True:
+            pkg.install("/tmp/rootfs")
+
+        """
+        cmd = "pisi it --ignore-comar"
+        if ignoredep == True:
+            cmd += " --ignore-dep" 
+        cmd += " -D %s -y %s  " % (basedir, self.filename)
+        os.system(cmd)
+
+class Packages:
+    """
+    This class allows us to define package groups.
+    Just pass the name of packages in a string separated 
+    with whitespace.
+
+    games = Packages("boswars tetris")
+    games.packages["boswars"].install("/tmp/rootfs")
+
+    or to install all
+
+    for pkgname, pkg in games.packages.items():
+       pkg.install("/tmp/rootfs")
+
+    """
+    def __init__(self, liststr = ""):
+        pkgs = liststr.split()
+        self.packages = {}
+        for pkg in pkgs:
+            self.packages[pkg] = PisiPackage(pkg)
+
+
 class RootFS:
     def __init__(self):
+        self.base = Packages(BASE)
         self.repo = "http://farm.pisilinux.org/.nofarm-repo/x86_64/pisi-index.xml.xz"
         self.cache = "/var/cache/pisi/packages"
         os.system("mkdir -p %s" % self.cache)
@@ -28,8 +109,6 @@ class RootFS:
         self.mknods()
         self.runCommand("pisi ar pisirepo %s" % self.repo)
 
-
-
     def mknods(self):
         self.runCommand("mknod /dev/console c 5 1")
         self.runCommand("mknod /dev/null c 1 3")
@@ -41,10 +120,8 @@ class RootFS:
         os.system(cmd)
 
     def installBase(self):
-        for pkg in self.pisipackages:
-            cmd = "%s --ignore-dep  %s" % (self.pisicmd, pkg)
-            print cmd
-            os.system(cmd)
+        for pkgname, pkg in self.base.packages.items():
+            pkg.install(self.rootdir)
 
     def runCommand(self, cmd):
         os.system("chroot %s  %s" % (self.rootdir, cmd) )
@@ -64,9 +141,6 @@ class RootFS:
         else:
             for m in self.mounts:
                 os.system("umount  %s%s" % (self.rootdir, m))
-
-    def download(self):
-        pass
 
     def clean(self):
         self.mountDirs(umount = True)
