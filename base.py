aef34e2796ea112da28e74488c393f2b6436f214
Ilker Manap <ilker@pisi.manap.se>
2015-03-19 16:32:20 +0100

    birden fazla repo kullanabilme eklendi

--- base.py
+++ base.py
@@ -5,41 +5,74 @@ ROOTFS=sys.argv[1]
 liste = open("paketler.txt").readlines()
 CACHE = "paket"
 
+class Indexes:
+    def __init__(self):
+        self.indexes = {}
+        self.priority = None
+
+    def setPriority(self, reponame):
+        self.priority = reponame
+
+    def addIndex(self, index):
+        self.indexes[index.name]  = index
+
+    def package(self, name):
+        selected = 0
+        pkg = None
+        repo = ""
+        if self.priority != None:
+            n = self.priority
+            if name in self.indexes[n].packages.keys():
+                return (n, self.indexes[n].packages[name])
+
+        for reponame, index in self.indexes.items():
+            if name in index.packages.keys():
+                if index.packages[name].release > selected:
+                    pkg = index.packages[name]
+                    selected = pkg.release
+                    repo = reponame
+        return (repo, pkg)
+
+
+
 class Index:
-    def __init__(self, repo):
+    def __init__(self, name, repo):
         self.url = repo
+        self.name = name
         self.base = repo[:repo.rfind("/")+1]
         self.content = ""
         self.packages = {}
         self.checkHash()
         try:
-            self.content = open("pisi-index.xml").read()
+            self.content = open("%s.pisi-index.xml" % self.name).read()
         except:
             self.retrieve()
         self.parse()
 
     def checkHash(self):
         import urllib2
-        if os.path.exists("index.sha1sum"):
+        if os.path.exists("%s.index.sha1sum" % self.name):
             yeniHash = urllib2.urlopen("%s.sha1sum" % self.url).readlines()[0]
-            eskiHash = open("index.sha1sum").readlines()[0]
+            eskiHash = open("%s.index.sha1sum" % self.name).readlines()[0]
             if yeniHash.strip() != eskiHash.strip():
                 self.retrieve()
                 f = open("index.sha1sum","w")
                 f.write(yeniHash)
                 f.close()
+            elif not (os.path.exists("%s.pisi-index.xml" % self.name)):
+                self.retrieve()
         else:
             yeniHash = urllib2.urlopen("%s.sha1sum" % self.url).readlines()[0]
             self.retrieve()
-            f = open("index.sha1sum","w")
+            f = open("%s.index.sha1sum" % self.name,"w")
             f.write(yeniHash)
             f.close()
 
 
 
     def retrieve(self):
-        os.system("wget %s" % self.url)
-        os.system("xz -d pisi-index.xml.xz")
+        os.system("wget %s -O %s.pisi-index.xml.xz" % (self.url, self.name))
+        os.system("xz -d %s.pisi-index.xml.xz" % self.name)
 
     def parse(self):
         from lxml import objectify as obj
@@ -52,13 +85,21 @@ class Index:
                         self.packages[pname] = Pkg(self.base, pname)
                     if d.tag == "PackageURI":
                         self.packages[pname].setFilename(d.text)
+                    if d.tag == "History":
+                        found = False
+                        for e in d.getchildren():
+                            if found == True:
+                                break
+                            if e.tag == "Update":
+                                self.packages[pname].setRelease(int(e.attrib["release"]))
+                                found = True
                     if d.tag == "DeltaPackages":
                         for e in d.getchildren():
                             if e.tag == "Delta":
                                 for f in e.getchildren():
                                     if f.tag == "PackageURI":
                                         self.packages[pname].addDelta(f.text)
-                        
+
     def report(self):
         for pname, pkg in self.packages.items():
             print "--------  ", pname, "  ----------------"
@@ -72,6 +113,7 @@ class Pkg:
         self.name = pname
         self.base = base
         self.filename = ""
+        self.release = -1
         self.deltas = []
         self.fname = self.filename.split("/")[-1]
 
@@ -81,6 +123,9 @@ class Pkg:
         print "Deltas"
         for i in self.deltas:
             print "  ", i
+
+    def setRelease(self, release):
+        self.release = release
 
     def setFilename(self, name):
         self.filename = name
@@ -106,7 +151,7 @@ class Pkg:
                 cmd = "wget %s/%s -O %s/%s" % (self.base, dlt, CACHE, d)
                 os.system(cmd)
                 p = Paket(d, target)
-            
+
 class Paket:
     def __init__(self, dosya_adi,  target):
         self.adi = dosya_adi
@@ -164,8 +209,6 @@ class Chroot:
             self.runOutside("rm -rf %s/usr/share/locale/[a-d][f-z]*" % self.root)
             self.runOutside("rm -rf %s/usr/share/locale/e[a-m,o-z]*" % self.root)
 
-
-        
     def dbus(self):
         #self.runOutside("mkdir -p %s/usr/lib/dbus-1.0/" % self.root)
         #self.runOutside("cp /usr/lib/dbus-1.0/dbus-daemon-launch-helper %s/usr/lib/dbus-1.0/." % self.root)
@@ -179,7 +222,7 @@ class Chroot:
         #self.runCommand('useradd -m -d /var/run/dbus -r -s /bin/false -u 18 -g 18 messagebus -c "D-Bus Message Daemon"')
         #self.runCommand("dbus-daemon --system")
         #self.runCommand("chmod o+x /usr/lib/dbus-1.0/dbus-daemon-launch-helper")
-            
+
     def mknods(self):
         self.runCommand("mkdir -m 755 -p /dev/pts")
         self.runCommand("mknod -m 666 /dev/null c 1 3")
@@ -227,11 +270,13 @@ class Chroot:
         else:
             self.rootlog.mesaj("chroot icinde calisacak : (%s) " % cmd)
             self.rootlog.mesaj(x)
-            
+
     def installPackages(self, paketListesi):
         for paket in open(paketListesi).readlines():
             paket = paket.strip()
-            self.index.packages[paket].install(self.root)
+            repo, pkg = self.index.package(paket)
+            print "%s reposundan %s kuruluyor" % (repo, paket)
+            pkg.install(self.root)
         self.index.packages["busybox"].fetch()
         Busybox("%s" % ( self.index.packages["busybox"].fname), self.root)
 
@@ -252,16 +297,21 @@ class Chroot:
         tagcmd = "docker tag -f %s %s:latest" % (imgtag, img)
         self.runOutside(dockercmd)
         self.runOutside(tagcmd)
-        
+
 if (__name__ == "__main__"):
     os.system("mkdir -p %s" % CACHE)
-    I = Index("http://farm.pisilinux.org/.nofarm-repo/x86_64/pisi-index.xml.xz")
-    x = Chroot(sys.argv[1], sys.argv[2], I)
+    I = Index("farm","http://farm.pisilinux.org/.nofarm-repo/x86_64/pisi-index.xml.xz")
+    J = Index("ilker","http://manap.se/pisi/pisi-index.xml.xz")
+    K = Indexes()
+    K.addIndex(I)
+    K.addIndex(J)
+    K.setPriority("ilker")
+    x = Chroot(sys.argv[1], sys.argv[2], K)
+
     x.addRepo("farm", "http://farm.pisilinux.org/.nofarm-repo/x86_64/pisi-index.xml.xz")
+    x.addRepo("ilker", "http://manap.se/pisi/pisi-index.xml.xz")
+
     x.addRepo("source","https://github.com/ertugerata/PisiLinux/raw/Pisi-2.0/pisi-index.xml.xz")
     #x.addRepo("source","/home/ertugrul/Works/PisiLinux/pisi-index.xml.xz")
     #x.buildpkg(sys.argv[3])
-    x.docker()
-
-
-    
+    #x.docker()
